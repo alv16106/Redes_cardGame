74d9ea938081493f13540dde0a5a98b672e98487
Rodrigo Alvarado <alv16106@uvg.edu.gt>
2019-09-11 00:35:17 -0600

    server and client doing nothing

--- chat/server.py
+++ chat/server.py
@@ -2,5 +2,46 @@ import socket
 import threading
 
 
-def accept():
-    pass
+class Server:
+
+    def __init__(self, port, host):
+        self.port = port
+        self.host = host
+        self.connections = []
+        self.address = []
+        self.threads = []
+        self.s = socket.socket()
+        self.s.bind((host, port))
+        self.s.listen(5)
+        self.accepting_connections()
+
+    def handle_client(self, clientsocket, addr):
+        clientsocket.send(str.encode("ls"))
+        while True:
+            msg = clientsocket.recv(1024)
+            # do some checks and if msg == disconnect: break:
+            print(addr, ' >> ', msg)
+            msg = input('SERVER >> ')
+            # GAME CODE
+            clientsocket.send(msg)
+        clientsocket.close()
+
+    def accepting_connections(self):
+        for c in self.connections:
+            c.close()
+
+        del self.connections[:]
+        del self.address[:]
+
+        while True:
+            conn, address = self.s.accept()
+            self.s.setblocking(1)  # prevents timeout
+
+            self.connections.append(conn)
+            self.address.append(address)
+            print("Connection has been established :" + address[0])
+            self.threads.append(threading.Thread(target=self.handle_client, args=(conn, address)))
+            
+
+if __name__ == "__main__":
+    server = Server(5000, "localhost")
