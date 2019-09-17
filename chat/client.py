c5da01363284aa0fed6b5fbdd2b0a4f8266817ff
Josrodjr <Josrodjr@gmail.com>
2019-09-16 20:30:29 -0600

    changes to client

--- chat/client.py
+++ chat/client.py
@@ -34,12 +34,41 @@ class Client:
             msg = input('envie algo: ')
             self.s.send(str.encode(msg))
 
+    # este lo hizo usted amiguito
     def join_room(self, args):
         self.s.send(pickle.dumps({'room': args, 'nick': self.nick}))
+        # yo creo que deberia ser asi
+        # self.s.send(
+        #     pickle.dumps(
+        #         {'code': 40, 'payload':
+        #             {'room': int(args)}}
+        #     )
+        # )
 
     def send_private(self, args):
         msg = input('Message: ')
-        self.s.send()
+        msg_dict = {}
+        msg_dict['code'] = 10
+        msg_dict['payload']['from'] = self.nick
+        msg_dict['payload']['to'] = args
+        msg_dict['payload']['msg'] = msg
+        self.s.send(pickle.dumps(msg_dict))
+
+    def execute_player(self, args):
+        self.s.send(pickle.dumps(
+            {'code': 20, 'payload': {'from': self.nick, 'vote': int(args)}}
+            ))
+
+    def kill_player(self, args):
+        self.s.send(pickle.dumps({'code': 30, 'payload': {'vote': int(args)}}))
+
+    def create_room(self, args):
+        self.s.send(
+            pickle.dumps(
+                {'code': 50, 'payload':
+                    {'roomname': args[0], 'max_players': int(args[1])}}
+            )
+        )
 
 if __name__ == "__main__":
     client = Client(5001, 'localhost')
